79cbf95b30e80bb1ec2823dfa70e6eaa2132d652
alepiaz <alessiopiazza117@gmail.com>
2024-05-18 18:03:12 +0100

    chore: removed parse args function

--- scripts/utils/setup.py
+++ scripts/utils/setup.py
@@ -2,34 +2,11 @@
 This module provides utility functions for the setup of the application.
 """
 
-from argparse import ArgumentParser, Namespace
 from typing import Any, Dict
 
 from scripts.console import Console
 from scripts.db_query_handler import DBQueryHandler
 from scripts.utils.config import get_key_dict
-
-
-def parse_arguments() -> Namespace:
-    """
-    Parses command-line arguments for the console application.
-
-    Returns:
-        argparse.Namespace: The parsed command-line arguments.
-    """
-    parser = ArgumentParser(description="Start the console with a given config file.")
-    parser.add_argument(
-        "--config",
-        type=str,
-        default="config/config.json",
-        help="The path to the config file.",
-    )
-    parser.add_argument(
-        "--delete-db",
-        action="store_true",
-        help="Delete the current database before starting the console.",
-    )
-    return parser.parse_args()
 
 
 def create_handler(config: Dict[str, Any]) -> DBQueryHandler:
