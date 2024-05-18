988c41b91ad83902fbcf922d446fcc69b59ea291
alepiaz <alessiopiazza117@gmail.com>
2024-05-18 18:02:33 +0100

    doc: updated docstring

--- setup.py
+++ setup.py
@@ -5,8 +5,9 @@ The application uses a configuration file to set up a console and a database que
 The console takes user input and uses the database query handler to interact with the database.
 """
 
+import argparse
 import logging
-from scripts.utils.setup import create_console, create_handler, parse_arguments
+from scripts.utils.setup import create_console, create_handler
 from scripts.utils.config import load_config
 
 
@@ -14,14 +15,13 @@ def main(config_path: str) -> None:
     """
     The main function of the application. It does the following:
 
-    1. Parses command-line arguments.
-    2. Loads the configuration file.
-    3. Creates an instance of the DBQueryHandler class.
-    4. Creates an instance of the Console class.
-    5. Starts the console.
+    1. Loads the configuration file.
+    2. Creates an instance of the DBQueryHandler class.
+    3. Creates an instance of the Console class.
+    4. Starts the console.
 
     If a RuntimeError occurs while starting the console,
-        it catches the exception and prints an error message.
+        it catches the exception and logs an error message.
     """
     config = load_config(config_path)
     handler = create_handler(config)
@@ -34,7 +34,6 @@ def main(config_path: str) -> None:
 
 
 if __name__ == "__main__":
-    import argparse
 
     parser = argparse.ArgumentParser(
         description="Start the console with a given config file."
