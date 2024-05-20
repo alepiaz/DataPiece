0da7a69c044ca2c230b20da4a9ce635ec5b0a39c
alepiaz <alessiopiazza117@gmail.com>
2024-05-20 22:46:21 +0100

    lint: pylint

--- scripts/db_query_handler.py
+++ scripts/db_query_handler.py
@@ -5,11 +5,9 @@ This module defines the DBQueryHandler class for handling database queries.
 import logging
 import os
 import sqlite3
-from logging import info
 
 from scripts.utils.config import get_key_str
-from scripts.utils.files import (is_readable_existing_file,
-                                 is_writeable_file_directory)
+from scripts.utils.files import is_readable_existing_file, is_writeable_file_directory
 
 
 class DBQueryHandler:
